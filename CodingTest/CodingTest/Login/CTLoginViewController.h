b04ccc604f9a3f5532db48ac88acc8aefa82fe78
Akshay Hegde <akshaynhegde@gmail.com>
2015-07-29 07:36:14 +0530

    A controller for the login screen

---
+++ CodingTest/CodingTest/Login/CTLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  CTLoginViewController.h
+//  CodingTest
+//
+//  Created by Akshay on 29/07/15.
+//  Copyright (c) 2015 Akshay Hegde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTLoginViewController : UIViewController
+
+@end
---
+++ CodingTest/CodingTest/Login/CTLoginViewController.m
@@ -0,0 +1,27 @@
+//
+//  CTLoginViewController.m
+//  CodingTest
+//
+//  Created by Akshay on 29/07/15.
+//  Copyright (c) 2015 Akshay Hegde. All rights reserved.
+//
+
+#import "CTLoginViewController.h"
+
+@interface CTLoginViewController ()
+
+@end
+
+@implementation CTLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
