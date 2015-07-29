82993400b88b204163b887932bb8e984e37f99b0
Akshay Hegde <akshaynhegde@gmail.com>
2015-07-29 22:55:20 +0530

    A collection controller class.

---
+++ CodingTest/CodingTest/Document Collection/CTDocumentCollectionViewController.m
@@ -0,0 +1,37 @@
+//
+//  CTDocumentCollectionViewController.m
+//  CodingTest
+//
+//  Created by Akshay on 29/07/15.
+//  Copyright (c) 2015 Akshay Hegde. All rights reserved.
+//
+
+#import "CTDocumentCollectionViewController.h"
+
+@interface CTDocumentCollectionViewController ()
+
+@end
+
+@implementation CTDocumentCollectionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
